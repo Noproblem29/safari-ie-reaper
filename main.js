9348aeeb19dee71e1eaa52ad5be666a54bd240c7
Noproblem29 <74529686+Noproblem29@users.noreply.github.com>
2021-12-27 11:31:05 +0500

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var app = express();
 var path = require("path");
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/safari-ie-reaper.github.io/index.html'));
+    res.sendFile(path.join(__dirname + '/static/index.html'));
 });
 app.listen(80);
